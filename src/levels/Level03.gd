76524fe81edfa13e7005ad981340fb95018d5d95
Jack Kapino <jackkapino@my.ccsu.edu>
2020-06-22 15:51:44 -0400

    Enemy Health update

--- src/levels/Level03.gd
+++ src/levels/Level03.gd
@@ -4,6 +4,23 @@ extends Node2D
 
 
 func _ready():
+	
+	var boss3_health =$RobotEnemy3/Health
+	var boss3_health_bar = $RobotEnemy3/HealthBar
+	
+	boss3_health.connect("changed",boss3_health_bar,"set_value")
+	boss3_health.connect("max_changed",boss3_health_bar,"set_max")
+	boss3_health.initialize()
+	
+	
+	var boss2_health =$RobotEnemy2/Health
+	var boss2_health_bar = $RobotEnemy2/HealthBar
+	
+	boss2_health.connect("changed",boss2_health_bar,"set_value")
+	boss2_health.connect("max_changed",boss2_health_bar,"set_max")
+	boss2_health.initialize()
+	
+	
 	var boss_health =$RobotEnemy/Health
 	var boss_health_bar = $RobotEnemy/HealthBar
 	
